6f315ace4410dc56a8fe18639aed6d0f675a27c5
Steven Chan <steven.s.chan@oracle.com>
2025-02-17 22:02:32 +0000

    update template: gcp-oci-adbs-quickstart

--- templates/gcp-oci-adbs-quickstart/main.tf
+++ templates/gcp-oci-adbs-quickstart/main.tf
@@ -1,13 +1,16 @@
 provider "google" {
   project = var.project
   region  = var.location
+  default_labels = {
+    tf-module = "gcp-oci-adbs-quickstart"
+    tf-source = "terraform-oci-multicloud-gcp"
+  }
 }
 
 locals {
   adbs_id   = "${var.adb_name}-${random_string.suffix.result}"
   adbs_name = "${var.adb_name}${random_string.suffix.result}"
   network_name = "${var.network_name}-${random_string.suffix.result}"
-  secret_name = "${local.adbs_id}_dba"
 }
 
 resource "random_string" "suffix" {
@@ -17,34 +20,11 @@ resource "random_string" "suffix" {
   numeric = true
 }
 
-resource "random_password" "admin_password" {
-  length           = 16
-  special          = true
-  override_special = "!#$%&*()-_=+[]{}<>:?"
-}
-
-# VPC
-# module "network" {
-#   source       = "terraform-google-modules/network/google//modules/vpc"
-#   version      = "10.0.0"
-#   network_name = local.network_name
-#   project_id   = var.project
-# }
-
 resource "google_compute_network" "this" {
   name = local.network_name
   project = var.project
+  auto_create_subnetworks = false
 }
-
-# Secret Manager
-# module "secret-manager" {
-#   source  = "GoogleCloudPlatform/secret-manager/google//modules/simple-secret"
-#   version = "~> 0.5"
-
-#   project_id  = var.project
-#   name        = local.secret_name
-#   secret_data = sensitive(random_password.admin_password.result)
-# }
 
 # Oracle Autonomous Database
 module "google_ora_adbs" {
@@ -59,28 +39,26 @@ module "google_ora_adbs" {
   cidr                   = var.cidr
   customer_email         = var.customer_email
   admin_password         = var.admin_password
-    # admin_password         = sensitive(random_password.admin_password.result)
-
 
   # Optional
-  # project                         = var.project
-  # display_name                    = var.display_name
-  # labels                          = var.labels
-  # compute_count                   = var.compute_count
-  # data_storage_size_tb            = var.data_storage_size_tb
-  # db_version                      = var.db_version
-  # db_edition                      = var.db_edition
-  # db_workload                     = var.db_workload
-  # is_auto_scaling_enabled         = var.is_auto_scaling_enabled
-  # license_type                    = var.license_type
-  # backup_retention_period_days    = var.backup_retention_period_days
-  # character_set                   = var.character_set
-  # is_storage_auto_scaling_enabled = var.is_storage_auto_scaling_enabled
-  # maintenance_schedule_type       = var.maintenance_schedule_type
-  # mtls_connection_required        = var.mtls_connection_required
-  # n_character_set                 = var.n_character_set
-  # operations_insights_state       = var.operations_insights_state
-  # private_endpoint_ip             = var.private_endpoint_ip
-  # private_endpoint_label          = var.private_endpoint_label
-  # deletion_protection             = var.deletion_protection
+  project                         = var.project
+  display_name                    = var.display_name
+  labels                          = var.labels
+  compute_count                   = var.compute_count
+  data_storage_size_tb            = var.data_storage_size_tb
+  db_version                      = var.db_version
+  db_edition                      = var.db_edition
+  db_workload                     = var.db_workload
+  is_auto_scaling_enabled         = var.is_auto_scaling_enabled
+  license_type                    = var.license_type
+  backup_retention_period_days    = var.backup_retention_period_days
+  character_set                   = var.character_set
+  is_storage_auto_scaling_enabled = var.is_storage_auto_scaling_enabled
+  maintenance_schedule_type       = var.maintenance_schedule_type
+  mtls_connection_required        = var.mtls_connection_required
+  n_character_set                 = var.n_character_set
+  operations_insights_state       = var.operations_insights_state
+  private_endpoint_ip             = var.private_endpoint_ip
+  private_endpoint_label          = var.private_endpoint_label
+  deletion_protection             = var.deletion_protection
 }
