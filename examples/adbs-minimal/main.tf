726cbfb4f9aeb3dd0dbe9d9dd6608d12bed3d5d6
Steven Chan <steven.s.chan@oracle.com>
2025-02-19 17:36:14 +0000

    Update adbs-minimal example

--- examples/adbs-minimal/main.tf
+++ examples/adbs-minimal/main.tf
@@ -1,12 +1,15 @@
 module "gcp-oci-adbs-quickstart" {
   # source = "github.com/oracle-quickstart/terraform-oci-multicloud-gcp//templates/gcp-oci-adbs-quickstart"
   source = "../../templates/gcp-oci-adbs-quickstart"
+  
   project = "example"
+  customer_email = "your_email@here"
+  admin_password = var.admin_password
+
   location = "europe-west2"  
   network_name = "example-vpc"
   cidr = "10.1.0.0/24"
-  customer_email = "your_email@here"
-  admin_password = "DoNotKeepThis$1234"
+
 }
 
 output "adbs_ocid" {
---
+++ examples/adbs-minimal/variables.tf
@@ -0,0 +1,5 @@
+variable "admin_password" {
+  description = "Admin password"
+  type = string
+  sensitive = true
+}
